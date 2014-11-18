b084356ff115067474af96e3f32ebe1f04029f53
RicPigeon <Richard_pig@hotmail.com>
2014-11-18 14:55:26 -0500

    Suite de l'interface avec la structure des objets

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -5,11 +5,33 @@
 var horaireControllers = angular.module('horaireControllers', []);
 
 horaireControllers.controller('GenerationHorairesCtrl', ['$scope',
-  function($scope, $) {
+  function($scope) {
     $scope.professeurs = 	[	{	
 								nom:'Beaudry',
 								prenom:'Eric',
-								cours:['INF3105','INF4230','DIC9315']
+								cours:[],
+								coursDesires: ['INF4204'],
+								mauvaiseEvaluation:['INF9999']
 							}
 						];
+						
+	var cours = 	{	
+						sigle : 'INF4230',
+						session:'A2014',
+						groupe: 10,
+						jour: 0,
+						heure : 1
+					};
+					
+	var cours2 =	{	
+						sigle : 'INF4730',
+						session:'A2014',
+						groupe: 20,
+						jour: 0,
+						heure : 1
+					};
+				
+	$scope.professeurs[0].cours.push(cours);
+	$scope.professeurs[0].cours.push(cours2);
+	
   }]);
